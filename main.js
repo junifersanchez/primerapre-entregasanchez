60ddc8db1956418574c21d561d25981c855def5f
junifersanchez <junisanchez0906@gmail.com>
2024-01-09 14:46:30 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// condicional//
+
+for (let i= 0; i < 100; i ++) {
+    console.log(i);
+}
+let= Number(prompt("ingresa un numero"));
+
+for(let i= 1; i <= 10; i++){
+    let resultado= nuemro * i;
+    alert(numero + " x " + i + " = " + resultado);
+}
